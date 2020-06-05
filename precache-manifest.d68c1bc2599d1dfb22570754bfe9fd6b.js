e6b05b73b62d3117d31b480f0201b1c7ed380224
Diwakar <ddn.dabbara@gmail.com>
2020-06-05 10:09:31 +0530

    Updates

---
+++ precache-manifest.d68c1bc2599d1dfb22570754bfe9fd6b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e456d504d4803a699053c7bb98fa84c9",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/index.html"
+  },
+  {
+    "revision": "f17400c48bcdf48323db",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/css/main.f8a354d1.chunk.css"
+  },
+  {
+    "revision": "08fc660ecb0db6191608",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/13.fef84c77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/13.fef84c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f711aca6edc986cc0d1",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/14.730a29b5.chunk.js"
+  },
+  {
+    "revision": "f17400c48bcdf48323db",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/main.cbceb2b0.chunk.js"
+  },
+  {
+    "revision": "0807a9b54a32f7c212d5",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerDailyMotion.5b5881ed.chunk.js"
+  },
+  {
+    "revision": "b1d7c8bca8080fbda688",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerFacebook.6d34ee41.chunk.js"
+  },
+  {
+    "revision": "9180bf027af34d6520ba",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerFilePlayer.d5e09b07.chunk.js"
+  },
+  {
+    "revision": "4848febb37a9f60afa2e",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerMixcloud.57c643f3.chunk.js"
+  },
+  {
+    "revision": "55650d71e193abea79c4",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerSoundCloud.783c9853.chunk.js"
+  },
+  {
+    "revision": "e525f7c4b09cf4b209c8",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerStreamable.d187e313.chunk.js"
+  },
+  {
+    "revision": "1fa9ded2dc1801d3243b",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerTwitch.c22f18b1.chunk.js"
+  },
+  {
+    "revision": "9df79e71986f30e9af8b",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerVidyard.f4b1d9c1.chunk.js"
+  },
+  {
+    "revision": "7d81ac07ef1197c36211",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerVimeo.3e1f32e3.chunk.js"
+  },
+  {
+    "revision": "b7fda4177350f9056ba8",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerWistia.78dc66b8.chunk.js"
+  },
+  {
+    "revision": "a7d652a1b0ba4c325eb0",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/reactPlayerYouTube.2cdb4c2f.chunk.js"
+  },
+  {
+    "revision": "14325dde248c0d52ffaf",
+    "url": "/dewalker07/frontend-developer-giveindia-challenge/static/js/runtime-main.abb3df57.js"
+  }
+]);
